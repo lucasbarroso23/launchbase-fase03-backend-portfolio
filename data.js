77195e482a091b8508f56ac4e9fdf0842b660809
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-12 10:09:15 -0300

    dados sendo recebidos do backend

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "AQ7pxuXhL64",
+        title: "Os pitbulls mais fofos do mundo",
+        duration: "3 min",
+        price: "Free"
+    },
+    {
+        id: "9rmPzPjefwU",
+        title: "Video de lontras brincando",
+        duration: "4 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "DtMuKjvhz5g",
+        title: "Filhotes de golden",
+        duration: "3 min",
+        price: "Free"
+    },
+    {
+        id: "BcyYQXTHIV8",
+        title: "Lontra acordando",
+        duration: "2 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "LIHwqDMgbhw",
+        title: "Pola bear baby",
+        duration: "3 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "TeCkm-BEZ-8",
+        title: "Cute lion cub",
+        duration: "3 min",
+        price: "Free"
+    },
+]
--- server.js
+++ server.js
@@ -2,21 +2,34 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const video = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/55262061?s=460&u=afd5e36a1928c45b184a72081834fe19a88b7046&v=4",
+        name: "Lucas Barroso",
+        role: "Aluno - Rocketseat",
+        description: 'Programador full-stack jr buscando oportunidade no mercado.<br><a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            {name: "Github", url: "https://github.com/lucasbarroso23"},
+            {name: "Linkdin", url: "https://www.linkedin.com/in/lucas-barroso23/"}
+        ]
+    }
+    return res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio")
+    return res.render("portfolio", { items: video })
 })
 
 server.listen(5000, function() {
